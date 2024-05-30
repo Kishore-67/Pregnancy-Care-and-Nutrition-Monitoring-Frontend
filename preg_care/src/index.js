b034af7f636d55cb5906b0df8a8fe30f304bd205
Kishore <kishore762k4@gmial.com>
2024-05-30 21:45:37 +0530

    4th commit

--- preg_care/src/index.js
+++ preg_care/src/index.js
@@ -3,11 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Login from './Authentication/Login'
+import SignUp from './Authentication/Signup'
+const appRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+ {
+    path: '/Login',
+    element:<Login/>
+  },
+  {
+    path:'/Signup',
+    element:<SignUp/>
+  }
+ 
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={appRouter} />
   </React.StrictMode>
 );
 
