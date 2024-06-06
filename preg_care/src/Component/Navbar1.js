404e51510cc587445a66bb82538484dbf05bc339
muruga21 <murugaperumal21062004@gmail.com>
2024-06-06 18:48:50 +0530

    Home Page

--- preg_care/src/Component/Navbar1.js
+++ preg_care/src/Component/Navbar1.js
@@ -1,23 +1,27 @@
+// src/components/Navbar1.js
+
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import '../Styles/Navbar1.css';
 
 export const Navbar1 = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="nav">
-      <div className="logo">MyApp</div>
+      <div className="logo">PregCare</div>
       <div
         className="menu-icon"
         onClick={() => {
           setMenuOpen(!menuOpen);
         }}
       >
+        &#9776; {/* Menu icon */}
       </div>
       <ul className={menuOpen ? "nav-links open" : "nav-links"}>
-        <li><NavLink to="/">Home</NavLink></li>
-        <li><NavLink to="/Hospital_reg">Register</NavLink></li>
-        <li><NavLink to="/Portal">LogIn</NavLink></li>
+        <li><NavLink to="/" onClick={() => setMenuOpen(false)}>Home</NavLink></li>
+        <li><NavLink to="/Hospital_reg" onClick={() => setMenuOpen(false)}>Register</NavLink></li>
+        <li><NavLink to="/Portal" onClick={() => setMenuOpen(false)}>LogIn</NavLink></li>
       </ul>
     </nav>
   );
